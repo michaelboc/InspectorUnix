3d36970338feccc1b0155d06a7d362f465f00653
Michael Boc <mhb5918@rit.edu>
2018-04-10 22:37:19 -0400

    Made some revisions and optimizations.

--- UnixForensicsProject.go
+++ UnixForensicsProject.go
@@ -4,7 +4,7 @@ main.go
 This file contains the source code of the main function of the forensic's
 tool InspectorUnix
 
-Version: Mar--2018
+Version: Apr-10-2018
 */
 
 
@@ -17,26 +17,30 @@ import (
 
 // Main routine of the program
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	var pathtodrive string
-	fmt.Println("Path to the suspect drive:")
-	fmt.Scan(&pathtodrive)
-	var wheretoput string
-	fmt.Println("Where to store drive image:")
-	fmt.Scan(&wheretoput)
-	var name string
-	fmt.Println("Desired name of image:")
-	fmt.Scan(&name)
-        go ImageDrive(pathtodrive,wheretoput,name, &wg )
-	var directory string
-	fmt.Println("Where to store memory dump:")
-	fmt.Scan(&directory)
-	var flashpath string
-	fmt.Println("Path to the external drive mount point:")
-	fmt.Scan(&flashpath)
-        makeoutfile()
-        fmt.Println("Summary of computer information created, but drive and memory imaging still running")
-        wg.Wait() 
-        ImageMemory( directory, flashpath )
+    var wg sync.WaitGroup
+    wg.Add(2)
+    // Memory Dump 
+    var directory string
+    fmt.Println("Where to store memory dump:")
+    fmt.Scan(&directory)
+    var flashpath string
+    fmt.Println( "Where the external drive is mounted:")
+    fmt.Scan(&flashpath)
+    go ImageMemory( directory, flashpath )
+    // Drive imaging
+    var pathtodrive string
+    fmt.Println("Path to the suspect drive:")
+    fmt.Scan(&pathtodrive)
+    var wheretoput string
+    fmt.Println("Where to store drive image:")
+    fmt.Scan(&wheretoput)
+    var name string
+    fmt.Println("Desired name of image:")
+    fmt.Scan(&name)
+    go ImageDrive( pathtodrive,wheretoput,name, &wg )
+    // System stats 
+    makeoutfile()
+    fmt.Println("Summary of computer information created, but drive and memory imaging still running")
+    // Wait till everybody is done 
+    wg.Wait() 
 }
