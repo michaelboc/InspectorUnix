7f2c572eb24dcd57783dbf1afa29b4aa37a5b377
Michael Boc <mhb5918@rit.edu>
2018-04-10 22:59:20 -0400

    g

--- UnixForensicsProject.go
+++ UnixForensicsProject.go
@@ -19,28 +19,31 @@ import (
 func main() {
     var wg sync.WaitGroup
     wg.Add(2)
-    // Memory Dump 
+    // Memory Dump
     var directory string
-    fmt.Println("Where to store memory dump:")
+    fmt.Println("Where to store memory dump?")
     fmt.Scan(&directory)
     var flashpath string
-    fmt.Println( "Where the external drive is mounted:")
+    fmt.Println( "Where the external drive is mounted?")
     fmt.Scan(&flashpath)
-    go ImageMemory( directory, flashpath )
+    var memSize string
+    fmt.Println( "How many megabytes of memory should be imaged?")
+    fmt.Scan(&memSize)
+    go ImageMemory( directory, flashpath, memSize, &wg )
     // Drive imaging
     var pathtodrive string
-    fmt.Println("Path to the suspect drive:")
+    fmt.Println("Path to the suspect drive?")
     fmt.Scan(&pathtodrive)
     var wheretoput string
-    fmt.Println("Where to store drive image:")
+    fmt.Println("Where to store drive image?")
     fmt.Scan(&wheretoput)
     var name string
-    fmt.Println("Desired name of image:")
+    fmt.Println("Desired name of image?")
     fmt.Scan(&name)
     go ImageDrive( pathtodrive,wheretoput,name, &wg )
-    // System stats 
+    // System stats
     makeoutfile()
     fmt.Println("Summary of computer information created, but drive and memory imaging still running")
-    // Wait till everybody is done 
-    wg.Wait() 
+    // Wait till everybody is done
+    wg.Wait()
 }
